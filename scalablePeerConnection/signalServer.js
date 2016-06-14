47082393011e9856324a32d1ea452f0760e9fdfc
Aaron Guan <zoom.aaron@gmail.com>
2016-06-14 16:39:31 +0800

    Add request to xirsys

--- scalablePeerConnection/signalServer.js
+++ scalablePeerConnection/signalServer.js
@@ -1,241 +1,248 @@
-var http = require("http");
-var https = require("https");
+//var http = require("http");
+//var https = require("https");
 var app = require("http").createServer();
 var io = require("socket.io")(app);
+var request = require("request");
 //user stores all the sockets
 var user = {};
 //room stores all the room id
 var room = {};
 var admin;
-var options = {
-		host: "https://service.xirsys.com/ice", 
+var configuration;
+
+var xirsys_details = {
 		ident : "qwerty",
 		secret : "53253e00-31f8-11e6-ae89-abe6e64b2707",
 		domain : "www.thegeeksnextdoor.co",
 		application : "default",
 		room : "default",
 		secure : 0,
-		port: 443
 }
 
-var prot = options.port == 443 ? https : http;
-var req = prot.request(options, function(data, status)
-		{
-	console.log(data);
-		});
+var opts = {
+	method: 'GET',
+	uri: 'https://service.xirsys.com/ice',
+	body: xirsys_details,
+	json: true
+};
+
+request.get(opts, function(error, response, body){
+	configuration = body.d;
+	console.log(iceServers);
+});
 
 app.listen(8080);
 
 io.on("connection", function(socket){
 
-	// new user login
-	socket.on("login", function(userName){
+		// new user login
+		socket.on("login", function(userName){
 
-		console.log("User " + userName + " logins");
+			console.log("User " + userName + " logins");
 
-		try {
+			try {
 			if (user[userName]){
 
-				socket.emit("login", {
-					type: "login",
-					userName: userName,
-					status: "fail"
-				});
+			socket.emit("login", {
+type: "login",
+userName: userName,
+status: "fail"
+});
 
-				console.log("Login unsuccessfully");
+			console.log("Login unsuccessfully");
 			} else{
-				user[userName] = socket;
-				user[userName].userName = userName;
+			user[userName] = socket;
+			user[userName].userName = userName;
 
-				socket.emit("login", {
-					type: "login",
-					userName: userName,
-					status: "success"
-				});
-			}}catch (e){
-				console.log(e);
-			}
-	})
+			socket.emit("login", {
+type: "login",
+userName: userName,
+status: "success"
+});
+}}catch (e){
+	console.log(e);
+}
+})
 
-	// a host create a new room
-	socket.on("createRoom", function(roomId){
+// a host create a new room
+socket.on("createRoom", function(roomId){
 		try {
-			/*if (room[roomId]){
-				socket.emit("createRoom", {
-					type: "createRoom",
-					userName: socket.userName,
-					room: roomId,
-					status: "fail"
-				});
-			} else{*/
-			room[roomId] = {};
-			room[roomId].roomId = roomId;
-			room[roomId].host = socket.userName;
-			user[socket.userName].room = roomId; 
-			user[socket.userName].join(roomId); 
-			admin.emit("host", {
-				type: "host",
-				host: socket.userName
-			});
-
+		/*if (room[roomId]){
 			socket.emit("createRoom", {
-				type: "createRoom",
-				userName: socket.userName,
-				room: roomId,
-				status: "success"
-			});
+type: "createRoom",
+userName: socket.userName,
+room: roomId,
+status: "fail"
+});
+} else{*/
+		room[roomId] = {};
+		room[roomId].roomId = roomId;
+		room[roomId].host = socket.userName;
+		user[socket.userName].room = roomId; 
+		user[socket.userName].join(roomId); 
+		admin.emit("host", {
+type: "host",
+host: socket.userName
+});
 
-			//}
-		}catch (e){
-			console.log(e);
-		}
-	})
+		socket.emit("createRoom", {
+type: "createRoom",
+userName: socket.userName,
+room: roomId,
+status: "success"
+});
 
-	// an user join a room
-	socket.on("joinRoom", function(roomId){
+//}
+}catch (e){
+	console.log(e);
+}
+})
+
+// an user join a room
+socket.on("joinRoom", function(roomId){
 		try {
-			if (room[roomId]){
+		if (room[roomId]){
 
-				socket.emit("host", {
-					type: "host",
-					host: room[roomId].host
-				});
+		socket.emit("host", {
+type: "host",
+host: room[roomId].host
+});
 
-				user[socket.userName].room = roomId;
-				user[socket.userName].join(roomId); 
+		user[socket.userName].room = roomId;
+		user[socket.userName].join(roomId); 
 
-				admin.emit("newUser", {
-					type: "newUser",
-					userName: socket.userName,
-					host:	room[roomId].host
-				});
+		admin.emit("newUser", {
+type: "newUser",
+userName: socket.userName,
+host:	room[roomId].host
+});
 
-				socket.emit("joinRoom", {
-					type: "joinRoom",
-					userName: socket.userName,
-					status: "success"
-				});
-			} else{
-				socket.emit("joinRoom", {
-					type: "joinRoom",
-					userName: socket.userName,
-					room: roomId,
-					status: "fail"
-				});
+		socket.emit("joinRoom", {
+type: "joinRoom",
+userName: socket.userName,
+status: "success"
+});
+} else{
+	socket.emit("joinRoom", {
+type: "joinRoom",
+userName: socket.userName,
+room: roomId,
+status: "fail"
+});
 
-			}}catch (e){
-				console.log(e);
-			}
-	})
+}}catch (e){
+	console.log(e);
+}
+})
 
-	//  an user send an offer to peer
-	socket.on("SDPOffer", function(sdpOffer){
+//  an user send an offer to peer
+socket.on("SDPOffer", function(sdpOffer){
 
 		console.log(sdpOffer.local + " is Sending offer to " + sdpOffer.remote);
 
 		try {
-			if (user[sdpOffer.remote]){
-				user[sdpOffer.remote].emit("SDPOffer", {
-					type: "SDPOffer",
-					local: sdpOffer.remote,
-					remote: sdpOffer.local,
-					offer: sdpOffer.offer
-				});
-			}else{
-				socket.emit("feedback", "Sending Offer: User does not exist or currently offline");
-			}} catch(e){
-				console.log(e);
-			}
-	})
+		if (user[sdpOffer.remote]){
+		user[sdpOffer.remote].emit("SDPOffer", {
+type: "SDPOffer",
+local: sdpOffer.remote,
+remote: sdpOffer.local,
+offer: sdpOffer.offer
+});
+		}else{
+		socket.emit("feedback", "Sending Offer: User does not exist or currently offline");
+		}} catch(e){
+		console.log(e);
+		}
+		})
 
-	//  an user send an answer to peer
-	socket.on("SDPAnswer", function(sdpAnswer){
+//  an user send an answer to peer
+socket.on("SDPAnswer", function(sdpAnswer){
 		console.log( sdpAnswer.remote + " is Receiving Answer from " + sdpAnswer.local);
 
 		try {
-			if (user[sdpAnswer.remote]){
-				user[sdpAnswer.remote].emit("SDPAnswer",{
-					type: "SDPAnswer",
-					local: sdpAnswer.remote,
-					remote: sdpAnswer.local,
-					answer: sdpAnswer.answer
-				});	
+		if (user[sdpAnswer.remote]){
+		user[sdpAnswer.remote].emit("SDPAnswer",{
+type: "SDPAnswer",
+local: sdpAnswer.remote,
+remote: sdpAnswer.local,
+answer: sdpAnswer.answer
+});	
 
-			}else{
-				socket.emit("feedback", "Sending Answer: User does not exist or currently offline");
-			}} catch(e){
-				console.log(e);
-			}
-	})
+		}else{
+		socket.emit("feedback", "Sending Answer: User does not exist or currently offline");
+		}} catch(e){
+		console.log(e);
+		}
+		})
 
-	//  an user send an ICECandidate to peer
-	socket.on("candidate", function(iceCandidate){
+//  an user send an ICECandidate to peer
+socket.on("candidate", function(iceCandidate){
 		console.log("an ice candidate is transfered");
 		console.log(iceCandidate.local);
 		console.log(iceCandidate.remote);
 		user[iceCandidate.remote].emit("candidate", {
-			type: "candidate",
-			local: iceCandidate.remote,
-			remote: iceCandidate.local,
-			candidate: iceCandidate.candidate
-		});
-	})
+type: "candidate",
+local: iceCandidate.remote,
+remote: iceCandidate.local,
+candidate: iceCandidate.candidate
+});
+		})
 
-	//	 an user disconnect
-	socket.on("disconnect", function(){
+//	 an user disconnect
+socket.on("disconnect", function(){
 		if (socket.userName){
-			admin.emit("disconnectedUser", {
-				type: "disconnectedUser",
-				userName: socket.userName,
-				host:	room[socket.room].host
-			});
-			socket.broadcast.to(socket.room).emit("message", {
-				type: "message",
-				action: "disconnect",
-				user: socket.userName,
-				content: ""
-			});
-			user[socket.userName] = null;
+		admin.emit("disconnectedUser", {
+type: "disconnectedUser",
+userName: socket.userName,
+host:	room[socket.room].host
+});
+		socket.broadcast.to(socket.room).emit("message", {
+type: "message",
+action: "disconnect",
+user: socket.userName,
+content: ""
+});
+		user[socket.userName] = null;
 		}
-	})
+		})
 
-	// a new peer connection is asked to be built
-	socket.on("newPeerConnection", function(userData){
+// a new peer connection is asked to be built
+socket.on("newPeerConnection", function(userData){
 		try {
-			console.log("host is " + userData.host + " and username is " + userData.userName);
-			user[userData.host].emit("initConnection", userData.userName);
-			//	console.log("User " + command[1] + " initialise connection to user " + command[2]);
+		console.log("host is " + userData.host + " and username is " + userData.userName);
+		user[userData.host].emit("initConnection", userData.userName);
+		//	console.log("User " + command[1] + " initialise connection to user " + command[2]);
 		} catch(e){
-			console.log(e);
+		console.log(e);
 		}
 
-	})
+		})
 
-	// a peer connection is asked to be deleted
-	socket.on("deletePeerConnection", function(userData){
+// a peer connection is asked to be deleted
+socket.on("deletePeerConnection", function(userData){
 		try {
-			console.log("peer is " + userData.peer + " and username is " + userData.userName);
-			user[userData.userName].emit("deleteConnection", userData.userName);
-			//	console.log("User " + command[1] + " initialise connection to user " + command[2]);
+		console.log("peer is " + userData.peer + " and username is " + userData.userName);
+		user[userData.userName].emit("deleteConnection", userData.userName);
+		//	console.log("User " + command[1] + " initialise connection to user " + command[2]);
 		} catch(e){
-			console.log(e);
+		console.log(e);
 		}
-	})
+		})
 
-	// a user send a message
-	socket.on("message", function(messageData){
+// a user send a message
+socket.on("message", function(messageData){
 		console.log(messageData.action);
 		socket.broadcast.to(socket.room).emit("message", messageData);
-	});
+		});
 
 
-	//	admin is connected
-	socket.on("admin", function(){
+//	admin is connected
+socket.on("admin", function(){
 		try {
-			admin = socket;
+		admin = socket;
 		} catch(e){
-			console.log(e);
+		console.log(e);
 		}
-	})
+		})
 
 })
