9b4e4725195cb498681eaf8d478e7b8babbf65eb
Aaron Guan <zoom.aaron@gmail.com>
2016-06-16 18:57:32 +0800

    Wire up goAdmin with route.go

--- scalablePeerConnection/go/route.go
+++ scalablePeerConnection/go/route.go
@@ -5,6 +5,7 @@ import (
     "encoding/json"
     "net"
     "bufio"
+    "sync"
 )
 
 type PeerInfo struct {
@@ -25,6 +26,12 @@ type User struct {
     Role string `json:"role"` //enum: "host", "user"
 }
 
+type Instruction struct {
+    Type string `json:"type"` //enum: "newPeerConnection" "deletePeerConnection"
+    Parent string `json:"parent"`
+    Child string `json:"child"` 
+}
+
 type Room struct {
     ID string `json:"roomID"`
     Users []User `json:"users"`
@@ -39,13 +46,22 @@ func (room *Room) getUsers() []User {
 }
 
 func (room *Room) removeUser(user User) {
-    room.Users = room.Users
     for i, u := range room.Users {
 	if u.Name == user.Name {
 	    room.Users = append(room.Users[:i], room.Users[i+1:]...) // The ... is essential
 	    return
 	}
     }
+}
+
+func (room *Room) getHost() User {
+    users := room.getUsers()
+    for _, u := range users {
+	if u.Role == "host" {
+	    return u
+	}
+    }
+    return User{}
 }
 
 const (
@@ -56,10 +72,14 @@ const (
 
 var rooms map[string]Room
 
+var mu sync.Mutex
+
+
 func main() {
     // Listen for incoming connections.
     listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
     queue := make(chan UserInfo, 10) // Buffered channel with capacity of 10
+    ins := make(chan Instruction, 10)
     rooms = make(map[string]Room)
     
     if err != nil {
@@ -79,13 +99,15 @@ func main() {
 	}
 	
 	// Handle connections in a new goroutine.
-	go handleRequest(conn, queue)
-	go handleTasks(conn, queue) // Potentially need to increase the number of workers
+	go handleRequests(conn, queue)
+	go handleTasks(queue, ins) // Potentially need to increase the number of workers
+	go handleInstructions(conn, ins)
     }
 }
 
 // Handles incoming requests.
-func handleRequest(conn net.Conn, queue chan<- UserInfo) {
+func handleRequests(conn net.Conn, queue chan<- UserInfo) {
+    fmt.Println("handleRequests is working")
     defer conn.Close()
     
     input := bufio.NewScanner(conn)
@@ -102,46 +124,91 @@ func handleRequest(conn net.Conn, queue chan<- UserInfo) {
     }
 }
 
-func handleTasks(conn net.Conn, queue <-chan UserInfo) {
+func handleTasks(queue <-chan UserInfo, ins chan<- Instruction) {
+    fmt.Println("handleTasks is working")
     for {
 	var userInfo UserInfo
 	userInfo = <- queue
 	
 	switch userInfo.Type {
-	case "newUser": fmt.Println("newUser") 
-	case "host": fmt.Println("host")
-	case "disconnectedUser": fmt.Println("disconnectedUser")
+	case "newUser": newUserHandler(userInfo, ins) 
+	case "host": newHostHandler(userInfo, ins)
+	case "disconnectedUser": disconnectHandler(userInfo, ins)
 	}
-	
-	fmt.Fprintf(conn, "Type: %s  User: %s  Room: %s  Host: %s", userInfo.Type, userInfo.User, userInfo.Room, userInfo.Host)
+	fmt.Printf("New task received -> Type: %s  User: %s  Room: %s\n", userInfo.Type, userInfo.User, userInfo.Room)
     }
 }
 
-func newUserHandler(userInfo UserInfo) {
+func newUserHandler(userInfo UserInfo, ins chan<- Instruction) {
+    fmt.Println("newUserHandler triggered")
     roomId := userInfo.Room
+    //mu.Lock()
     if room, exist := rooms[roomId]; exist {
+	fmt.Println(room.getHost())
 	user := User{Name: userInfo.User, Role: "user"}
 	room.addUser(user)
+	
+	/* Send out instructions */
+	/* TODO: may need to separate out this part */
+	host := room.getHost()
+	if host.Role == "host" { 
+	    ins <- Instruction{Type:"newPeerConnection", Parent: host.Name, Child: user.Name}
+	} else {
+	    fmt.Println("ERR: Host doesn't exist")
+	}
+	fmt.Println(room.getUsers())
     }
+    //mu.Unlock()
 }
 
-func newHostHandler(userInfo UserInfo) {
+func newHostHandler(userInfo UserInfo, ins chan<- Instruction) {
+    fmt.Println("newHostHandler triggered")
     roomId := userInfo.Room
-    if room, exist := rooms[roomId]; !exist {
+    //mu.Lock()
+    if _, exist := rooms[roomId]; !exist {
 	user := User{Name: userInfo.User, Role: "host"}
 	users := make([]User, 0)
 	users = append(users, user)
 	room := Room{ID: roomId, Users: users}
 	rooms[roomId] = room;
+	fmt.Println(room.getUsers())
     }
+    //mu.Unlock()
 }
 
-func disconnectHandler(userInfo UserInfo) {
+func disconnectHandler(userInfo UserInfo, ins chan<- Instruction) {
+    fmt.Println("disconnectHandler triggered")
     roomId := userInfo.Room
     if room, exist := rooms[roomId]; exist {
-	room.removeUser(userInfo.User)
+	user := User{Name:userInfo.User, Role:"user"}
+	room.removeUser(user)
+	
+	/* Send out instruction */
+	host := room.getHost()
+	
+	if host.Role == "host" {
+	    ins <- Instruction{Type:"deletePeerConnection", Parent: host.Name, Child: user.Name}
+	} else {
+	    fmt.Println("ERR: Host doesn't exist")
+	}
+	
 	if len(room.getUsers())==0 {
 	    delete(rooms, roomId)
 	}
+	fmt.Println(room.getUsers())
+    }
+}
+
+func handleInstructions(conn net.Conn, ins <-chan Instruction) {
+    fmt.Println("handleInstructions is working")
+    for {
+	instruction := <- ins
+	str, err := json.Marshal(instruction)
+	if err != nil {
+	    fmt.Println("Error listening:", err.Error())
+	    continue
+	}
+	fmt.Fprintf(conn, "%s", string(str))
+	fmt.Println("Instruction Sent")
     }
 }
--- scalablePeerConnection/goAdmin.js
+++ scalablePeerConnection/goAdmin.js
@@ -8,46 +8,46 @@ var PORT = 8888;
 
 var go = new net.Socket();
 go.connect(PORT, HOST, function() {
-    console.log('Connected to Go server: ' + HOST + ':' + PORT);
-    
-    // Send signal to signalServer.js that admin starts to work
-    signalSocket.emit("admin");
-    console.log('admin.js starts to work');
-    
-    //var text = JSON.stringify(peer);
-    //client.write(text+'\n');
+  console.log('Connected to Go server: ' + HOST + ':' + PORT);
+  
+  // Send signal to signalServer.js that admin starts to work
+  signalSocket.emit("admin");
+  console.log('admin.js starts to work');
+  
+  //var text = JSON.stringify(peer);
+  //client.write(text+'\n');
 });
 
 go.on('data', function(data) {
-    console.log('Go: ' + data.toString());
+  console.log('Go: ' + data.toString());
+  var jsonStr = data.toString();
+  var res = JSON.parse(jsonStr);
+  console.log(res);
+  switch (res.type) {
+    case "newPeerConnection": 
+      signalSocket.emit("newPeerConnection", res); break;
+    case "deletePeerConnection":
+      signalSocket.emit("deletePeerConnection", res); break;
+  }
+  
 });
 
 go.on('close', function() {
-    console.log('Connection to Go server is closed');
-    go.destroy();
+  console.log('Connection to Go server is closed');
+  go.destroy();
 });
 
-
 signalSocket.on("host", function(userData){
-    console.log(userData.host);
-    //taskSocket.emit("host", userData);
+  console.log(userData);
+  go.write(JSON.stringify(userData)+'\n');
 });
 
 signalSocket.on("newUser", function(userData){
-    console.log(userData);
-    go.write(JSON.stringify(userData)+'\n');
+  console.log(userData);
+  go.write(JSON.stringify(userData)+'\n');
 });
 
 signalSocket.on("disconnectedUser", function(userData){
-    //taskSocket.emit("disconnectedUser", userData);
+  console.log(userData);
+  go.write(JSON.stringify(userData)+'\n');
 });
-
-/*
-taskSocket.on("newPeerConnection", function(userData){
-    signalSocket.emit("newPeerConnection", userData);
-})
-
-taskSocket.on("deletePeerConnection", function(userData){
-    signalSocket.emit("deletePeerConnection", userData);
-})
-*/
